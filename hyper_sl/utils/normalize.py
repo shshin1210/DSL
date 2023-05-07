692f0b9a3c61df353182998bdf8778097d9ae993
Suhyun Shin <80568500+shshin1210@users.noreply.github.com>
2023-05-07 12:10:59 +0900

    new projector min max values

--- hyper_sl/utils/normalize.py
+++ hyper_sl/utils/normalize.py
@@ -6,8 +6,12 @@ def normalization(arg, data):
         normalize projector coord xy
     """
     
-    x_gt_min, x_gt_max = torch.tensor([-0.0024 - 1e-4], device= arg.device), torch.tensor([0.0027 + 1e-4], device= arg.device)
-    y_gt_min, y_gt_max = torch.tensor([-0.0033 - 1e-4], device= arg.device), torch.tensor([0.0005 + 1e-4], device= arg.device)
+    # x_gt_min, x_gt_max = torch.tensor([-0.0024 - 1e-4], device= arg.device), torch.tensor([0.0027 + 1e-4], device= arg.device)
+    # y_gt_min, y_gt_max = torch.tensor([-0.0033 - 1e-4], device= arg.device), torch.tensor([0.0005 + 1e-4], device= arg.device)
+    
+    x_gt_min, x_gt_max = torch.tensor([-0.0026 - 1e-4], device= arg.device), torch.tensor([0.0024 + 1e-4], device= arg.device)
+    y_gt_min, y_gt_max = torch.tensor([-0.0027 - 1e-4], device= arg.device), torch.tensor([7.7517e-05 + 1e-4], device= arg.device)
+    
     
     x_gt_minmax = (data[...,0] - x_gt_min.unsqueeze(dim = 1)) / (x_gt_max.unsqueeze(dim = 1) - x_gt_min.unsqueeze(dim = 1)) 
     y_gt_minmax = (data[...,1] - y_gt_min.unsqueeze(dim = 1)) / (y_gt_max.unsqueeze(dim = 1) - y_gt_min.unsqueeze(dim = 1))
@@ -21,8 +25,12 @@ def un_normalization(data):
         unnormalize projector coord xy
     """
     
-    x_min, x_max = -0.0024 - 1e-4, 0.0027 + 1e-4
-    y_min, y_max = -0.0033 - 1e-4, 0.0005 + 1e-4
+    # x_min, x_max = -0.0024 - 1e-4, 0.0027 + 1e-4
+    # y_min, y_max = -0.0033 - 1e-4, 0.0005 + 1e-4
+    
+    x_min, x_max = -0.0026 - 1e-4, 0.0024 + 1e-4
+    y_min, y_max = -0.0027 - 1e-4, 7.7517e-05 + 1e-4
+    
     
     data_unnorm_x = data[...,0] * (x_max - x_min) + x_min
     data_unnorm_y = data[...,1] * (y_max - y_min) + y_min
