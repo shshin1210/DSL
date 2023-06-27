fddaf80deab28edd22813e4b20eb253cfd2a246c
Suhyun Shin <80568500+shshin1210@users.noreply.github.com>
2023-06-27 15:09:32 +0900

    captured image file processing

--- calibration/dg_calibration/file_process.py
+++ calibration/dg_calibration/file_process.py
@@ -25,7 +25,7 @@ def crop(arg, data):
     
     return data
 
-def undistort(img_dir, int_mtx, dist_mtx, bit):
+def undistort(img_dir, int_mtx, dist_mtx, bit, bool):
     """
         input : image directory(where image is located)
                 intrinsic, extrinsic matrix, bit
@@ -34,7 +34,12 @@ def undistort(img_dir, int_mtx, dist_mtx, bit):
     """
 
     img = cv2.imread(os.path.join(img_dir), -1) / bit
-    img_undistort = cv2.undistort(img, int_mtx, dist_mtx)*255.
+    
+    if bool == True:
+        img_undistort = cv2.undistort(img, int_mtx, dist_mtx)*255.
+    else: 
+        img_undistort = img * 255.
+    
     return img_undistort
 
 def save_img(save_dir, img, fn):
@@ -44,21 +49,21 @@ def save_img(save_dir, img, fn):
     """
     cv2.imwrite(os.path.join(save_dir, fn), img)
 
-def main(arg):
+def main(arg, bool):
     """
         capture images with 650 ~ 450 nm sequence in calibration0~4 folder
         
         **test_fn = directory name
         
-        **if wavelength sequence changes, change wvls
+        **True : undistort image
+          False : no undistortion to image
     """
     
-    # wvls = np.arange(450, 660, 50)[::-1]
     wvls = np.arange(450, 660, 50)
     
     cam_int, cam_dist = calibrated_params.bring_params(arg.calibration_param_path, "cam")
     
-    test_fn = "test_2023_05_29_13_01"
+    test_fn = "test_2023_06_24_13_40"
     img_test_path = "C:/Users/owner/Documents/GitHub/Scalable-Hyp-3D-Imaging/calibration/dg_calibration/" + test_fn
     
     # files for each wvls
@@ -70,7 +75,7 @@ def main(arg):
         
         for idx2, inner_file in enumerate(inner_files):
             img_path = os.path.join(img_test_path, file, inner_file)
-            img_undistort = undistort(img_path, cam_int, cam_dist, 65535.)
+            img_undistort = undistort(img_path, cam_int, cam_dist, 65535., bool)
             img_undistort_crop = crop(arg, img_undistort)
             
             # where to save cropped and undistorted image
@@ -78,12 +83,14 @@ def main(arg):
             if not os.path.exists(save_dir):
                 os.makedirs(save_dir)
 
-            save_img(save_dir, img_undistort_crop, '%dnm'%wvls[idx] + '_undistort.png')
+            save_img(save_dir, img_undistort_crop, '%dnm.png'%wvls[idx])
+
 
 if __name__ == "__main__":
     argument = Argument()
     arg = argument.parse()
 
-    main(arg)
+    bool = False # True : undistort image / False : no undistortion to image
+    main(arg, bool)
 
     print('end')
