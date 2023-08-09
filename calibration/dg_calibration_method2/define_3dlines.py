d13cf0f2d8978cdc558a0a17115a70678f94e075
Suhyun Shin <80568500+shshin1210@users.noreply.github.com>
2023-08-09 22:58:10 +0900

    diffraction calibration definining 3d points and 3d lines for data

--- calibration/dg_calibration_method2/define_3dlines.py
+++ calibration/dg_calibration_method2/define_3dlines.py
@@ -5,49 +5,151 @@ sys.path.append('C:/Users/owner/Documents/GitHub/Scalable-Hyp-3D-Imaging')
 import numpy as np
 import matplotlib.pyplot as plt
 from hyper_sl.utils.ArgParser import Argument
+import torch
 
-def visualization(front_world_3d_pts, back_world_3d_pts, order):
-    fig = plt.figure()
-    ax = plt.axes(projection = '3d')
-
-    ax.scatter(front_world_3d_pts[order,:,::10,0].flatten(), front_world_3d_pts[order,:,::10,1].flatten(), front_world_3d_pts[order,:,::10,2].flatten(), s =3.5)
-    ax.scatter(back_world_3d_pts[order,:,::10,0].flatten(), back_world_3d_pts[order,:,::10,1].flatten(), back_world_3d_pts[order,:,::10,2].flatten(), s= 3.5)
-
-    ax.set_xlim([-0.15,0.15])
-    ax.set_ylim([-0.1,0.1])
-    ax.set_zlim([-0.,0.6])
-
-    plt.title('%d order'%order)
-    plt.show()
-
-def dir_visualization(dir_vec, front_world_3d_pts, order):
-    fig = plt.figure()
-    ax = plt.axes(projection = '3d')
-
-    scale = 1
-    for i in range(0, 5*2303):
-        # if (dir_vec[order, i, 2] < 0.) or (front_world_3d_pts[order,i,2] < 0.):
-        #     continue
-        # else:
-        start = [front_world_3d_pts[order,i,0], front_world_3d_pts[order,i,1], front_world_3d_pts[order,i,2]]
-
-        X_d = [start[0], start[0] + scale*dir_vec[order,i,0]]
-        Y_d = [start[1], start[1] + scale*dir_vec[order,i,1]]
-        Z_d = [start[2], start[2] + scale*dir_vec[order,i,2]]
+class Define3dLines():
+    def __init__(self, arg, front_world_3d_pts, mid_world_3d_pts, back_world_3d_pts):
+        # argument
+        self.arg = arg
+        self.wvls = np.arange(450, 660, 50)
+        self.pts_num = (arg.proj_H // 10) * (arg.proj_W // 10)
+        self.m_list = arg.m_list
         
-        ax.plot(X_d,Y_d,Z_d, color = 'red', linewidth = 1)
+        # 3d points : m, wvl, # pts, 3
+        self.front_world_3d_pts = front_world_3d_pts
+        self.mid_world_3d_pts = mid_world_3d_pts
+        self.back_world_3d_pts = back_world_3d_pts
         
-    ax.scatter(front_world_3d_pts[order,i,0].flatten(), front_world_3d_pts[order,i,1].flatten(), front_world_3d_pts[order,i,2].flatten(), s = 1.)
+    def define3d_lines(self):
+        # direction vector
+        dir_vec = self.direction_vector()        
+        
+        # delete outliers of direction vector
+        idx = (self.front_world_3d_pts[...,2] > 0.) * (self.mid_world_3d_pts[...,2] > 0.) * (self.back_world_3d_pts[...,2] > 0.)
+        for i in range(arg.m_num*len(self.wvls)*self.pts_num):
+            if idx[i] == False:
+                dir_vec[i,:] = 0. # let outlier's direction vector be Zero
+
+        dir_vec = dir_vec.reshape(self.arg.m_num, len(self.wvls), self.pts_num,3)
+        
+        return dir_vec
+    
+    def direction_vector(self):
+        
+        dir_vec = self.mean_method() # m, wvl, # pts, 3
+        # dir_vec = self.opt_method() 
+        
+        return dir_vec
+    
+    
+    def mean_method(self):
+        points = np.stack(self.front_world_3d_pts, self.mid_world_3d_pts, self.back_world_3d_pts) # 3pts, m, wvl, # pts, xyz
+
+        # Calculate the average of the last two points
+        average_point = np.mean(points[1:], axis=0) # m, wvl, # pts, xyz
+
+        # Calculate the direction vector
+        direction_vector = average_point - points[0] # m, wvl, # pts, xyz
+
+        # Normalize the direction vector
+        normalized_direction = direction_vector / np.linalg.norm(direction_vector, axis = 3)[:,:,:,np.newaxis]
+
+        return normalized_direction
+    
+    def opt_method(self):
+        initial_point_value = torch.tensor(self.front_world_3d_pts)
+        initial_dir_value = torch.tensor(self.back_world_3d_pts - self.front_world_3d_pts)
+
+        initial_value = torch.hstack((initial_point_value,initial_dir_value))
+
+        opt_param = torch.tensor(initial_value, dtype= torch.float, requires_grad=True, device= "cuda")
+
+        # loss ftn
+        loss = 0
+        losses = []
+
+        lr = 1e-3 # 1e-3
+        decay_step = 2500 # 1000
+        epoch = 15000
+
+        num_pts = 3
+        points = torch.stack((self.front_world_3d_pts, self.mid_world_3d_pts, self.back_world_3d_pts), device = "cuda")
+
+        optimizer = torch.optim.Adam([opt_param], lr = lr)
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer=optimizer, step_size=decay_step, gamma = 0.8)
+
+        for i in range(epoch):
+            loss = 0
+            # distance between 3 points
+            for k in range(3):
+                dist = self.distance(opt_param[:3], points[k], opt_param[3:]) # p, q, dir
+                loss += dist
+
+            optimizer.zero_grad()
+            loss.backward()
             
-    ax.set_xlim([-0.15,0.15])
-    ax.set_ylim([-0.1,0.1])
-    ax.set_zlim([0.35,0.65])
+            losses.append(loss.item() / num_pts)
+            optimizer.step()
+            scheduler.step()
 
-    plt.title('%d order'%order)
-    plt.show()
+            if i % 1000 == 0:
+                print(f" Opt param value : {opt_param}, Epoch : {i}/{epoch}, Loss: {loss.item() / num_pts}, LR: {optimizer.param_groups[0]['lr']}")
+        
+        return opt_param[:3], opt_param[3:]
+    
+    def distance(self, q, p, dir_vec):
+        """
+            p : point on the line
+            q : 3d point
+        
+        """
+        cross = torch.cross(q - p, dir_vec, dim = 3)
+        distance = torch.norm(cross, p = 2, dim = 3) / torch.norm(dir_vec, p = 2, dim = 3)
+        
+        return distance    
+
+    def visualization(self, order):
+        fig = plt.figure()
+        ax = plt.axes(projection = '3d')
+
+        ax.scatter(self.front_world_3d_pts[order,:,::10,0].flatten(), self.front_world_3d_pts[order,:,::10,1].flatten(), self.front_world_3d_pts[order,:,::10,2].flatten(), s =3.5)
+        ax.scatter(self.mid_world_3d_pts[order,:,::10,0].flatten(), self.mid_world_3d_pts[order,:,::10,1].flatten(), self.mid_world_3d_pts[order,:,::10,2].flatten(), s= 3.5)
+        ax.scatter(self.back_world_3d_pts[order,:,::10,0].flatten(), self.back_world_3d_pts[order,:,::10,1].flatten(), self.back_world_3d_pts[order,:,::10,2].flatten(), s= 3.5)
+
+        ax.set_xlim([-0.15,0.15])
+        ax.set_ylim([-0.1,0.1])
+        ax.set_zlim([-0.,0.6])
+
+        plt.title('%d order'%order)
+        plt.show()
+
+    def dir_visualization(self, dir_vec, order):
+        fig = plt.figure()
+        ax = plt.axes(projection = '3d')
+
+        scale = 1
+        for i in range(0, 5*2303):
+            start = [self.front_world_3d_pts[order,i,0], self.front_world_3d_pts[order,i,1], self.front_world_3d_pts[order,i,2]]
+
+            X_d = [start[0], start[0] + scale*dir_vec[order,i,0]]
+            Y_d = [start[1], start[1] + scale*dir_vec[order,i,1]]
+            Z_d = [start[2], start[2] + scale*dir_vec[order,i,2]]
+            
+            ax.plot(X_d,Y_d,Z_d, color = 'red', linewidth = 1)
+            
+        ax.scatter(self.front_world_3d_pts[order,i,0].flatten(), self.front_world_3d_pts[order,i,1].flatten(), self.front_world_3d_pts[order,i,2].flatten(), s = 1.)
+                
+        ax.set_xlim([-0.15,0.15])
+        ax.set_ylim([-0.1,0.1])
+        ax.set_zlim([0.35,0.65])
+
+        plt.title('%d order'%order)
+        plt.show()
 
     
-def main(arg):
+if __name__ == "__main__":
+    argument = Argument()
+    arg = argument.parse()
     
     # wvl
     wvls = np.arange(450, 660, 50)
@@ -55,35 +157,19 @@ def main(arg):
     
     # bring 3d points
     front_world_3d_pts = np.load('./front_world_3d_pts.npy').reshape(arg.m_num, len(wvls), pts_num, 3)
+    mid_world_3d_pts = np.load('./mid_world_3d_pts.npy').reshape(arg.m_num, len(wvls), pts_num, 3)
     back_world_3d_pts = np.load('./back_world_3d_pts.npy').reshape(arg.m_num, len(wvls), pts_num, 3)
 
     # bring proj pts
     proj_pts = np.load('./proj_pts.npy')
     
+    defining_3dlines = Define3dLines(arg, front_world_3d_pts, mid_world_3d_pts, back_world_3d_pts)
+    
+    # define direction vector
+    dir_vec = defining_3dlines.define3d_lines()
+    
     # visualization of second order
-    # visualization(front_world_3d_pts, back_world_3d_pts, 2)
+    defining_3dlines.visualization(2)
 
-    front_world_3d_pts_reshape = front_world_3d_pts.reshape(-1, 3)
-    back_world_3d_pts_reshape = back_world_3d_pts.reshape(-1, 3)
-
-    # delete outliers
-    dir_vec_reshape = back_world_3d_pts_reshape - front_world_3d_pts_reshape
-    idx = (front_world_3d_pts_reshape[...,2] > 0.) * ( back_world_3d_pts_reshape[...,2] > 0.)
-    
-    for i in range(3*5*pts_num):
-        if idx[i] == False:
-            dir_vec_reshape[i,:] = 0.
-    
-    dir_vec = dir_vec_reshape.reshape(3,5*2303,3)
-    front_world_3d_pts = front_world_3d_pts.reshape(3, 5*2303,3)
-    
     # visualization of direction vector lines
-    # dir_visualization(dir_vec, front_world_3d_pts, 2)
-
-    # define lines
-    
-if __name__ == "__main__":
-    argument = Argument()
-    arg = argument.parse()
-    
-    main(arg)
+    defining_3dlines.dir_visualization(dir_vec, 2)
--- calibration/dg_calibration_method2/define_3dpoints.py
+++ calibration/dg_calibration_method2/define_3dpoints.py
@@ -8,100 +8,92 @@ import numpy as np
 import point_process
 import matplotlib.pyplot as plt
 
-def get_3d_points(points_3d_dir):
-    points_3d = np.load(points_3d_dir)
-    
-    return points_3d
 
-def get_proj_px(pattern_npy_dir):
-    proj_px = np.load(pattern_npy_dir)
-    
-    return proj_px
-
-def get_detected_pts(arg, point_dir, data_dir, wvls, i, proj_px, flg):
-    detected_pts_dir = point_dir + '/pattern_%04d'%i
-    detected_pts = point_process.point_process(arg, data_dir, detected_pts_dir, wvls, i, proj_px, flg)
-    detected_pts = (np.round(detected_pts)).astype(np.int32)
-    
-    return detected_pts
-
-def visualization(world_3d_pts):
-    fig = plt.figure()
-    ax = plt.axes(projection = '3d')
-    
-    ax.scatter(world_3d_pts[:,:,:,0].flatten(), world_3d_pts[:,:,:,1].flatten(), world_3d_pts[:,:,:,2].flatten())
-
-    ax.set_xlim([-0.15,0.15])
-    ax.set_ylim([-0.1,0.1])
-    ax.set_zlim([-0.5,0.6])
-
-    plt.show()
-    
-def world3d_pts(arg, date, flg):
-    # Variable Date
-    date = date
-    front = flg
-    
-    wvls = np.arange(450, 660, 50)
-    wvls_num = len(wvls)
-    total_px = (arg.proj_H//10)*(arg.proj_W//10) 
-    
-    if front == True:
-        position = "front"
-    else:
-        position = "back"
-    
-    # directory
-    main_dir = "./calibration/dg_calibration_method2/2023%s_data"%date
-    data_dir = os.path.join(main_dir, position)
-    
-    # detected points dir
-    point_dir = data_dir + '_points'
-    # spectralon 3d points
-    points_3d_dir = os.path.join(main_dir , "spectralon_depth_%s_%s.npy"%(date,position))
-    # pattern npy 
-    pattern_npy_dir = "./calibration/dg_calibration_method2/grid_npy"
-
-    # 3d points
-    points_3d = get_3d_points(points_3d_dir)
-    
-    # New arrays : m, wvl, # px(=1), 2
-    world_3d_pts = np.zeros(shape=(arg.m_num, wvls_num, total_px, 3))
-    world_3d_pts_reshape = world_3d_pts.reshape(-1, total_px, 3) # m * wvl, # px, 3
-    # projector sensor plane pxs : #px, 2
-    proj_pts = np.zeros(shape=(total_px, 2))
-    
-    for i in range(len(os.listdir(pattern_npy_dir))):
-        # proj pixel center points
-        pattern_dir = os.path.join(pattern_npy_dir, "pattern_%05d.npy"%i)
+class Define3dPoints():
+    def __init__(self, arg, date, position):
         
-        # projector pixel points
-        proj_px = get_proj_px(pattern_dir)
-        proj_pts[i] = proj_px
+        # arguments
+        self.arg = arg
+        self.date = date
+        self.position = position
         
-        # detected pts
-        detected_pts = get_detected_pts(arg, point_dir, data_dir, wvls, i, proj_px, flg) # m, wvl, 2
-        detected_pts_reshape = detected_pts.reshape(-1, 2) # (x, y 순)
+        # directory 
+        self.main_dir = "./calibration/dg_calibration_method2/2023%s_data"%self.date
+        self.data_dir = os.path.join(self.main_dir, self.position)
+        self.point_dir = self.data_dir + '_points' # detected points dir
+        self.points_3d_dir = os.path.join(self.main_dir , "spectralon_depth_%s_%s.npy"%(self.date,self.position)) # spectralon 3d points
+        self.pattern_npy_dir = "./calibration/dg_calibration_method2/grid_npy" # pattern npy 
+        
+    def get_3d_points(self):
+        points_3d = np.load(self.points_3d_dir)
+        
+        return points_3d
+
+    def get_proj_px(self):
+        proj_px = np.load(self.pattern_npy_dir)
+        
+        return proj_px
+
+    def get_detected_pts(self, wvls, i, proj_px):
+        detected_pts_dir = self.point_dir + '/pattern_%04d'%i
+        detected_pts = point_process.point_process(self.arg, self.data_dir, detected_pts_dir, wvls, i, proj_px, self.position)
+        detected_pts = (np.round(detected_pts)).astype(np.int32)
+        
+        return detected_pts
+
+    def visualization(self, world_3d_pts):
+        fig = plt.figure()
+        ax = plt.axes(projection = '3d')
+        
+        ax.scatter(world_3d_pts[:,:,:,0].flatten(), world_3d_pts[:,:,:,1].flatten(), world_3d_pts[:,:,:,2].flatten())
+
+        ax.set_xlim([-0.15,0.15])
+        ax.set_ylim([-0.1,0.1])
+        ax.set_zlim([-0.5,0.6])
+
+        plt.show()
+        
+    def world3d_pts(self):
+
+        wvls = np.arange(450, 660, 50)
+        wvls_num = len(wvls)
+        total_px = (self.arg.proj_H//10)*(self.arg.proj_W//10) 
+
+        # 3d points
+        points_3d = self.get_3d_points()
+        
+        # New arrays : m, wvl, # px(=1), 2
+        world_3d_pts = np.zeros(shape=(self.arg.m_num, wvls_num, total_px, 3))
+        world_3d_pts_reshape = world_3d_pts.reshape(-1, total_px, 3) # m * wvl, # px, 3
+        proj_pts = np.zeros(shape=(total_px, 2)) # projector sensor plane pxs : # px, 2
+        
+        for i in range(len(os.listdir(self.pattern_npy_dir))):
+            # projector pixel points
+            proj_px = self.get_proj_px()
+            proj_pts[i] = proj_px
             
-        world_3d_pts_reshape[:,i,:] = points_3d[detected_pts_reshape[:,1], detected_pts_reshape[:,0]]
+            # detected pts
+            detected_pts = self.get_detected_pts(self.arg, wvls, i, proj_px) # m, wvl, 2
+            detected_pts_reshape = detected_pts.reshape(-1, 2) # (x, y 순)
+                
+            world_3d_pts_reshape[:,i,:] = points_3d[detected_pts_reshape[:,1], detected_pts_reshape[:,0]]
 
-    return world_3d_pts_reshape, proj_pts
-
-def main(arg):
+        return world_3d_pts_reshape, proj_pts
+        
+    
+if __name__ == "__main__":
+    argument = Argument()
+    arg = argument.parse()
+    
     date = "0728"
+    position = ""
 
-    front_world_3d_pts_reshape, proj_pts = world3d_pts(arg, date, flg=True)
-    back_world_3d_pts_reshape, proj_pts = world3d_pts(arg, date, flg=False)
+    front_world_3d_pts_reshape, proj_pts = Define3dPoints(arg, date, "front").world3d_pts()
+    mid_world_3d_pts_reshape, proj_pts = Define3dPoints(arg, date, "mid").world3d_pts()
+    back_world_3d_pts_reshape, proj_pts = Define3dPoints(arg, date, "back").world3d_pts()
     
     np.save('./front_world_3d_pts.npy', front_world_3d_pts_reshape)
     np.save('./back_world_3d_pts.npy', back_world_3d_pts_reshape)
     np.save('./proj_pts.npy', proj_pts)
     
     print('end')
-    
-    
-if __name__ == "__main__":
-    argument = Argument()
-    arg = argument.parse()
-    
-    main(arg)
