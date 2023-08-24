aebb3763702580575050855eb2de44a503226d12
Suhyun Shin <80568500+shshin1210@users.noreply.github.com>
2023-08-24 17:39:32 +0900

    homography transf

--- calibration/dg_calibration_method2/homography_transform.py
+++ calibration/dg_calibration_method2/homography_transform.py
@@ -1,87 +1,103 @@
 import numpy as np
 import cv2, os,sys
+import scipy.io as io
+import torch
 
 sys.path.append('C:/Users/owner/Documents/GitHub/Scalable-Hyp-3D-Imaging')
 import matplotlib.pyplot as plt
 from hyper_sl.utils.ArgParser import Argument
-
+from point_process import PointProcess
 
 class HomographyTransform():
     def __init__(self, arg, date, position):
         # arguments
         self.arg = arg
+        self.date = date
         self.wvls = np.array([430, 450, 480, 500, 520, 550, 580, 600, 620, 650, 660])
-
-        # directory
-        self.processed_data_dir = './calibration/dg_calibration_method2/2023%s_data/%s_processed'%(date, position)
-        self.homography_data_dir = './calibration/dg_calibration_method2/2023%s_data/homography'%date
-        self.final_dir = './calibration/dg_calibration_method2/2023%s_data/%s_processed_homo'%(date, position)
+        self.position = position
+        self.total_px = arg.total_px
         
-    def load_img(self, type):
+        # directory 
+        self.main_dir = "./calibration/dg_calibration_method2/2023%s_data"%self.date
+        self.data_dir = os.path.join(self.main_dir, self.position)
+        self.processed_data_dir = os.path.join(self.main_dir, "%s_processed"%self.position)
+        self.points_dir = self.data_dir + '_points' # detected points dir
+        self.points_3d_dir = os.path.join(self.main_dir , "spectralon_depth_%s_%s.npy"%(self.date,self.position)) # spectralon 3d points
+        self.pattern_npy_dir = "./calibration/dg_calibration_method2/grid_npy" # pattern npy 
+        self.final_dir = './calibration/dg_calibration_method2/2023%s_data/%s_processed_homo'%(date, position)
+
+        # point datas
+        self.filter_zero_orders, self.no_filter_zero_orders = self.load_points()
+        np.save('./filter_zero_orders.npy', self.filter_zero_orders)
+        np.save('./no_filter_zero_orders.npy', self.no_filter_zero_orders)
+        
+    def get_proj_px(self, dir):
         """
-            load homography images
-            type : band pass filter holder / wheel / with no filter
+            get (u, v) coords for projected projector sensor plane
+        """
+        proj_px = np.load(dir)
+        
+        return proj_px
+    
+    def get_detected_pts(self, i, proj_px):
+        """
+            get preprocessed detection points : m, wvl, # points, 3(xyz)
+            
+            returns detected points
             
         """
+        detected_pts_dir = self.points_dir + '/pattern_%04d'%i
+        processed_img_dir = self.processed_data_dir + '/pattern_%04d'%i
+        detected_pts = PointProcess(self.arg, self.data_dir, detected_pts_dir, processed_img_dir, self.wvls, i, proj_px, self.position).point_process()
+        detected_pts = (np.round(detected_pts)).astype(np.int32)
         
-        if type == "wheel":
-            image = cv2.imread(os.path.join(self.homography_data_dir, 'wheel.png'), cv2.IMREAD_GRAYSCALE)
-        elif type == "holder":
-            image = cv2.imread(os.path.join(self.homography_data_dir, 'holder.png'), cv2.IMREAD_GRAYSCALE)
-        else:
-            image = cv2.imread(os.path.join(self.homography_data_dir, 'plain.png'), cv2.IMREAD_GRAYSCALE)
-        
-        return image
+        return detected_pts
     
-    def detect_compute(self, image1, image2):
-        # Step 1: Detect and match features using ORB detector as an example
-        orb = cv2.ORB_create()
-        keypoints1, descriptors1 = orb.detectAndCompute(image1, None)
-        keypoints2, descriptors2 = orb.detectAndCompute(image2, None)
+    def load_points(self):
+        # projector points
+        proj_pts = np.zeros(shape=(self.total_px, 2)) # projector sensor plane pxs : # px, 2
 
-        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-        matches = bf.match(descriptors1, descriptors2)
-
-        return keypoints1, descriptors1, keypoints2, descriptors2, matches
-    
-    def transform(self, image1, image2):
+        # 전체 패턴 개수 : 464개의 points / wvl + no filter : 11개 
+        filter_zero_orders = np.zeros(shape=(self.total_px, len(self.wvls), 2))
+        no_filter_zero_orders = np.zeros(shape=(self.total_px, 2))
         
-        # Parameters for the checkerboard (inner corners)
-        rows = 6 # Example value
-        cols = 9 # Example value
+        for i in range(len(os.listdir(self.pattern_npy_dir))):
+            # projector pixel points
+            proj_px = self.get_proj_px(os.path.join(self.pattern_npy_dir,"pattern_%05d.npy"%i))
+            proj_pts[i] = proj_px
+            
+            # band pass filter zero orders
+            detected_pts = self.get_detected_pts(i, proj_px) # m, wvl, 2 // 패턴의 개수 
+            centroids = io.loadmat(os.path.join(self.points_dir, "pattern_%04d"%i, "no_fi_centroid.mat"))['centers']
+            
+            if len(centroids) == 0:
+                no_filter_zero_orders[i] = np.zeros(shape=(2))
+            else:
+                no_filter_zero_orders[i] = centroids
+                
+            filter_zero_orders[i] = detected_pts[1].squeeze()
+            
+        return filter_zero_orders, no_filter_zero_orders
 
-        # Step 1: Detect checkerboard corners
-        ret1, corners1 = cv2.findChessboardCorners(image1, (rows, cols))
-        ret2, corners2 = cv2.findChessboardCorners(image2, (rows, cols))
-
-        if ret1 and ret2:
-            # Optional: Refine the corner detections
-            criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-            cv2.cornerSubPix(image1, corners1, (11,11), (-1,-1), criteria)
-            cv2.cornerSubPix(image2, corners2, (11,11), (-1,-1), criteria)
-
-        # # Step 1: Detect and match features using ORB detector as an example
-        # keypoints1, descriptors1, keypoints2, descriptors2, matches = self.detect_compute(image1, image2)
+    def transform(self, src_pts, dst_pts):
         
-        # # Sort matches based on their distances
-        # matches = sorted(matches, key = lambda x:x.distance)
-
-        # # Take the top N matches
-        # N = 50
-        # best_matches = matches[:N]
-
-        # # Get the matching keypoints
-        # src_pts = np.float32([ keypoints1[m.queryIdx].pt for m in best_matches ]).reshape(-1,1,2)
-        # dst_pts = np.float32([ keypoints2[m.trainIdx].pt for m in best_matches ]).reshape(-1,1,2)
-
-
+        # Get the matching keypoints
+        src_pts = np.float32(src_pts).reshape(-1,1,2)
+        dst_pts = np.float32(dst_pts).reshape(-1,1,2)
+        
+        # get rid of zero points?
+        mask_valid = (src_pts > 0.)* (dst_pts > 0.)
+        valid_pairs = mask_valid[:, 0, 0]
+        
+        # Mask the src_pts and dst_pts
+        src_pts_masked = src_pts[valid_pairs]
+        dst_pts_masked = dst_pts[valid_pairs]
+        
         # Step 2: Compute the homography matrix
-        # H, _ = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
-        H, _ = cv2.findHomography(corners1, corners2, cv2.RANSAC, 5.0)
-
+        H, _ = cv2.findHomography(src_pts_masked, dst_pts_masked, cv2.RANSAC, 5.0)
 
         return H
-    
+
     def warp_img(self, image, H):
         height, width = arg.cam_H, arg.cam_W
         warpped_image = cv2.warpPerspective(image, H, (width, height))
@@ -89,58 +105,38 @@ class HomographyTransform():
         return warpped_image
         
     def homography_transformation(self):
-        # Load homography images
-        wheel_img = self.load_img("wheel")
-        holder_img = self.load_img("holder")
-        plain_img = self.load_img("plain_img")
-        
-        # Homography 
-        # wheel_to_plain_H = self.transform(wheel_img, plain_img)
-        wheel_to_plain_H = self.transform(plain_img, wheel_img)
-
-        holder_to_plain_H = self.transform(plain_img, holder_img)
         
         # warp perspective for all images & same
-        # wheel : 430nm ~ 550nm
-        # holder : 580nm ~ 660nm
         files = os.listdir(self.processed_data_dir)
-        # make dir
+        
+        # make processed homo directory
         if not os.path.exists(self.final_dir):
             os.makedirs(self.final_dir)
         
         for i in range(len(self.wvls)):
-            for dir in files:
-                if i < 6:
-                    wvl_dir = os.path.join(self.processed_data_dir, dir)
-                    wvl_img = cv2.imread(os.path.join(wvl_dir, '%snm.png'%self.wvls[i]))
-                    
-                    warpped_img = self.warp_img(wvl_img, wheel_to_plain_H)
-                    
-                    new_dir = os.path.join(self.final_dir, dir)
-                    if not os.path.exists(new_dir):
-                        os.makedirs(new_dir)
-                    
-                    cv2.imwrite(os.path.join(new_dir, '%snm.png'%self.wvls[i]), warpped_img)
-                    
-                else:
-                    wvl_dir = os.path.join(self.processed_data_dir, dir)
-                    wvl_img = cv2.imread(os.path.join(wvl_dir, '%snm.png'%self.wvls[i]))
-                    
-                    warpped_img = self.warp_img(wvl_img, holder_to_plain_H)
-                    
-                    new_dir = os.path.join(self.final_dir, dir)
-                    if not os.path.exists(new_dir):
-                        os.makedirs(new_dir)
-                    
-                    cv2.imwrite(os.path.join(new_dir, '%snm.png'%self.wvls[i]), warpped_img)
+            # wvl 마다 detection 된 point load / homography wavelength 마다 구함
+            H = self.transform(self.filter_zero_orders[:,i], self.no_filter_zero_orders) # wvl 마다의 homography
             
+            for dir in files: # pattern directory
+                wvl_dir = os.path.join(self.processed_data_dir, dir)
+                wvl_img = cv2.imread(os.path.join(wvl_dir, '%snm.png'%self.wvls[i]))
+                
+                warpped_img = self.warp_img(wvl_img, H)
+                
+                new_dir = os.path.join(self.final_dir, dir)
+                if not os.path.exists(new_dir):
+                    os.makedirs(new_dir)
+                
+                cv2.imwrite(os.path.join(new_dir, '%snm.png'%self.wvls[i]), warpped_img)
+
+        
         
 if __name__ == "__main__":
     argument = Argument()
     arg = argument.parse()
     
     bool = False # True : undistort image / False : no undistortion to image
-    date = "0821" # date of data
+    date = "0822" # date of data
     
     # Homography    
     HomographyTransform(arg, date, position="front").homography_transformation()
