9288edd17d65b2a158d046436a0969ae329c3735
shshin1210 <shinsuhyun1210@gmail.com>
2023-08-26 06:12:42 +0000

    jarvis code update 230826

--- hyper_sl/utils/ArgParser.py
+++ hyper_sl/utils/ArgParser.py
@@ -18,7 +18,7 @@ class Argument:
 
 		self.parser.add_argument('--image_formation_dir', type = str, default="./dataset/image_formation/result")
 		self.parser.add_argument('--precomputed_proj_coordinates_dir', type = str, default="./dataset/image_formation/xy_vproj")
-		self.parser.add_argument('--dg_intensity_dir', type=str, default='./calibration/radiometric_cal')
+		self.parser.add_argument('--dg_intensity_dir', type=str, default='./dataset/image_formation')
 		# self.parser.add_argument('--dat_dir', type = str, default = './dataset/image_formation/dat')
 		self.parser.add_argument('--dat_dir', type = str, default= "./dataset/image_formation/dat/method2/interpolated")
 		self.parser.add_argument('--illum_data_dir', type = str, default= "./dataset/image_formation/illum_data.npy")
