4ce4db91105b3a94461aa824b739a12e96c56fbd
Suhyun Shin <80568500+shshin1210@users.noreply.github.com>
2023-10-05 10:27:26 +0900

    spectralon added for depth non linear interpolation

--- calibration/position_calibration/depth_interpolation.py
+++ calibration/position_calibration/depth_interpolation.py
@@ -9,15 +9,15 @@ from data_process import DataProcess
 from scipy import interpolate
 from scipy.io import loadmat, savemat
 from tqdm import tqdm
-
+from scipy.optimize import curve_fit
 
 class DepthInterpolation():
-    def __init__(self, arg, date, front_peak_illum_idx, mid_peak_illum_idx, back_peak_illum_idx):
+    def __init__(self, arg, date, front_peak_illum_idx, mid_peak_illum_idx, mid2_peak_illum_idx, mid3_peak_illum_idx, back_peak_illum_idx):
         
         # arguments
         self.date = date
         self.depth = arg.depth_list
-        self.positions = ["front", "mid", "back"]
+        self.positions = ["front", "mid","mid2","mid3", "back"]
         self.cam_H, self.cam_W = arg.cam_H, arg.cam_W
         
         self.m_list = arg.m_list
@@ -37,15 +37,20 @@ class DepthInterpolation():
         # dir
         self.data_dir = "./calibration/position_calibration/2023%s/%s_depth/spectralon"
         self.dat_dir = "./dataset/image_formation/dat/method3"
-        
+        self.npy_dir = "./calibration/position_calibration/2023%s/npy_data"%date
+
         # peak illumination index informations
         self.front_peak_illum_idx = front_peak_illum_idx # 3(m order), wvl(2), HxW
         self.mid_peak_illum_idx = mid_peak_illum_idx
+        self.mid2_peak_illum_idx = mid2_peak_illum_idx
+        self.mid3_peak_illum_idx = mid3_peak_illum_idx
         self.back_peak_illum_idx = back_peak_illum_idx
         
         # ppg graph
         self.max_data_back = np.load('./calibration/position_calibration/2023%s/npy_data/max_data_%s.npy'%(date, "back"))
         self.max_data_mid = np.load('./calibration/position_calibration/2023%s/npy_data/max_data_%s.npy'%(date, "mid"))
+        self.max_data_mid2 = np.load('./calibration/position_calibration/2023%s/npy_data/max_data_%s.npy'%(date, "mid2"))
+        self.max_data_mid3 = np.load('./calibration/position_calibration/2023%s/npy_data/max_data_%s.npy'%(date, "mid3"))
         self.max_data_front = np.load('./calibration/position_calibration/2023%s/npy_data/max_data_%s.npy'%(date, "front"))
 
     def depth_interpolation(self):
@@ -53,31 +58,47 @@ class DepthInterpolation():
             depth interpolation from 600mm to 900mm at 1mm interval
         
         """
-        all_position_peak_illum_idx = np.stack((self.front_peak_illum_idx, self.mid_peak_illum_idx, self.back_peak_illum_idx), axis = 0)
+        all_position_peak_illum_idx = np.stack((self.front_peak_illum_idx, self.mid_peak_illum_idx, self.mid2_peak_illum_idx, self.mid3_peak_illum_idx, self.back_peak_illum_idx), axis = 0)
         depth = np.array([self.get_depth(position) for position in self.positions]) * 1e+3
         
         depth_peak_illum_idx = np.zeros(shape=(len(self.depth_arange), len(self.m_list), len(self.wvl_list), len(self.sample_pts_flatt)))
+        # for visualization
+        # vis_list = []
+        # vis_range = []
         
         for m in range(len(self.m_list)):
             for w in range(len(self.wvl_list)):   
                 for idx, i in enumerate(self.sample_pts_flatt):
                     # previous depth range and interpolated depth range
-                    depth_range = np.round((np.array([depth[p,i] for p in range(len(self.positions))]))).astype(np.int32)
-                    new_depth_range = np.arange(depth_range[0], depth_range[2] + 1, 1) 
+                    depth_range = np.round(np.array([depth[p,i] for p in range(len(self.positions))])).astype(np.int32)
+                    new_depth_range = np.arange(depth_range[0], depth_range[-1] + 1, 1) 
                     
                     # exceptions
-                    if (depth_range[0] > depth_range[1]) or (depth_range[1] > depth_range[2]) or (depth_range[0] > depth_range[2]) or (depth_range[0] > self.depth_start) or (depth_range[2] < self.depth_end):
+                    if (False in (np.sort(depth_range) == depth_range)) or (depth_range[0] > self.depth_start) or (depth_range[-1] < self.depth_end):
                         depth_range = np.array([self.depth_start, self.depth_start + 100, self.depth_end])
                         new_depth_range = np.arange(self.depth_start, self.depth_end + 1, 1)
                         
                     # only bring 600mm to 900mm depths
                     idx_start, idx_end = np.where(new_depth_range == self.depth_start)[0][0], np.where(new_depth_range == self.depth_end)[0][0]
-                    interp_depth = self.cubic_interpolation(new_depth_range, depth_range, all_position_peak_illum_idx[:,m,w,i].reshape(3, -1), 2)[idx_start:idx_end+1]
+                    # interp_depth = self.cubic_interpolation(new_depth_range, depth_range, all_position_peak_illum_idx[:,m,w,i].reshape(len(depth_range), -1), 4)[idx_start:idx_end+1]
+                    
+                    # non linear fitting
+                    params, cov = curve_fit(self.fitting_function, depth_range, all_position_peak_illum_idx[:,m,w,i].reshape(len(depth_range), -1).flatten(), maxfev = 50000000, method='trf')
+                    interp_depth = self.fitting_function(new_depth_range, *params)[idx_start:idx_end+1]
                     
                     # save depth
                     depth_peak_illum_idx[:, m, w, idx] = interp_depth
                     
+                    # for visualization
+                    # vis_depth = self.fitting_function(new_depth_range, *params)
+                    # vis_list.append(vis_depth)
+                    # vis_range.append(new_depth_range)
+                    
         return depth_peak_illum_idx
+    
+    # fitting function
+    def fitting_function(self, x, a, b, c):
+        return a*(x**b) + c
     
     # bring depth values
     def get_depth(self, position):
@@ -104,9 +125,9 @@ class DepthInterpolation():
             shape :
             depth(600mm-900mm at 1mm interval), 2(m=-1 or 1 and 0), wvl(430nm, 660nm), sample pts
         """
-        # depth_peak_illum_idx = self.depth_interpolation()
-        # np.save('./depth_peak_illum_idx.npy', depth_peak_illum_idx)
-        depth_peak_illum_idx = np.load('./depth_peak_illum_idx.npy')
+        depth_peak_illum_idx = self.depth_interpolation()
+        np.save(os.path.join(self.npy_dir,'./depth_peak_illum_idx.npy'), depth_peak_illum_idx)
+        depth_peak_illum_idx = np.load(os.path.join(self.npy_dir,'./depth_peak_illum_idx.npy'))
         
                                                     # depth, m order (-1 or 1 and zero), wvl(430, 660), pts
         depth_peak_illum_idx_final = np.zeros(shape=(len(self.depth_arange), 2, len(self.wvl_list), self.sample_pts.shape[0])) 
@@ -134,10 +155,14 @@ if __name__ == "__main__":
     
     # front_peak_illum_idx = DataProcess(arg, date, "front").get_first_idx()
     # mid_peak_illum_idx = DataProcess(arg, date, "mid").get_first_idx()
+    # mid2_peak_illum_idx = DataProcess(arg, date, "mid2").get_first_idx()
+    # mid3_peak_illum_idx = DataProcess(arg, date, "mid3").get_first_idx()
     # back_peak_illum_idx = DataProcess(arg, date, "back").get_first_idx()
     
     front_peak_illum_idx = np.load("./calibration/position_calibration/2023%s/npy_data/peak_illum_idx_front.npy"%date)
     mid_peak_illum_idx = np.load("./calibration/position_calibration/2023%s/npy_data/peak_illum_idx_mid.npy"%date)
+    mid2_peak_illum_idx = np.load("./calibration/position_calibration/2023%s/npy_data/peak_illum_idx_mid2.npy"%date)
+    mid3_peak_illum_idx = np.load("./calibration/position_calibration/2023%s/npy_data/peak_illum_idx_mid3.npy"%date)
     back_peak_illum_idx = np.load("./calibration/position_calibration/2023%s/npy_data/peak_illum_idx_back.npy"%date)
     
-    DepthInterpolation(arg, date, front_peak_illum_idx, mid_peak_illum_idx, back_peak_illum_idx).get_depth_peak_illum()
+    DepthInterpolation(arg, date, front_peak_illum_idx, mid_peak_illum_idx, mid2_peak_illum_idx, mid3_peak_illum_idx, back_peak_illum_idx).get_depth_peak_illum()
